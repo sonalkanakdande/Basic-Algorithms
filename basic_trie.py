2363450d9aba7d193ff8a407a23656a0911ed506
sonalkanakdande <44235033+sonalkanakdande@users.noreply.github.com>
2019-06-23 20:35:05 +0800

    Thid is basic trie in pythin.

---
+++ basic_trie.py
@@ -0,0 +1,76 @@
+class TrieNode(object):
+    def __init__(self):
+        self.is_word = False
+        self.children = {}
+
+
+class Trie(object):
+    def __init__(self):
+        self.root = TrieNode()
+
+    def add(self, word):
+        """
+        Add `word` to trie
+        """
+        current_node = self.root
+
+        for char in word:
+            if char not in current_node.children:
+                current_node.children[char] = TrieNode()
+            current_node = current_node.children[char]
+
+        current_node.is_word = True
+
+    def exists(self, word):
+        """
+        Check if word exists in trie
+        """
+        current_node = self.root
+
+        for char in word:
+            if char not in current_node.children:
+                return False
+            current_node = current_node.children[char]
+
+        return current_node.is_word
+
+basic_trie = {
+    # a and add word
+    'a': {
+        'd': {
+            'd': {'word_end': True},
+            'word_end': False},
+        'word_end': True},
+    # hi word
+    'h': {
+        'i': {'word_end': True},
+        'word_end': False}}
+
+
+print('Is "a"   a word: {}'.format(basic_trie['a']['word_end']))
+print('Is "ad"  a word: {}'.format(basic_trie['a']['d']['word_end']))
+print('Is "add" a word: {}'.format(basic_trie['a']['d']['d']['word_end']))
+
+
+def is_word(word):
+    """
+    Look for the word in `basic_trie`
+    """
+    current_node = basic_trie
+
+    for char in word:
+        if char not in current_node:
+            return False
+
+        current_node = current_node[char]
+
+    return current_node['word_end']
+
+
+# Test words
+test_words = ['ap', 'add']
+for word in test_words:
+    if is_word(word):
+        print('"{}" is a word.'.format(word))
+    else:
+        print('"{}" is not a word.'.format(word))
